df2c04eb6d98eba24455dfe2fa369662749a841d
Samuel Medina <smdoomed@gmail.com>
2024-10-08 22:01:04 -0600

    Seccion hero

--- views/index.php
+++ views/index.php
@@ -1,7 +1,25 @@
 <?php include 'partials/header.php'; ?>
 <?php include 'partials/navbar.php'; ?>
 <main>
-    <section class="mw--1440">
+    <section class="mw--1440 row align-items-center position-relative">
+        <div class="home__hero">
+            <div class="p-5">
+                <div class="p-5">
+                    <h2>Antes de comprar una casa piensa en <span class="text--green">SOC</span></h2>
+                    <p class="text--grey text--sm">
+                        Somos el Broker líder en asesoría hipotecaria en México
+                    </p>
+                </div>
+            </div>
+            <div>
+                <img src="<?php echo __ROOT__; ?>/public/img/home/hero.png" alt="hero" class="w-100">
+            </div>
+        </div>
+        <div id="conoceMasHero">
+            <a href="<?php echo __ROOT__; ?>/contacto">
+                Conoce más
+            </a>
+        </div>
     </section>
 </main>
 <?php include 'partials/footer.php'; ?>
